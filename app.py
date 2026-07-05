f37f7ef86aa0837099b057b5d1e384073ab2961d
vigneshwaranp-source <vigneshwaran.p@heromotocorp.com>
2026-07-05 20:17:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ elif "GEMINI_API_KEY" in os.environ:
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-pro')
 else:
     st.sidebar.warning("⚠️ **API Key Missing**")
     model = None
